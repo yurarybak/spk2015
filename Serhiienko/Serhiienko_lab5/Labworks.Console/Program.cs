652a9d20249e4b9cf649d4795b16be41f5959263
VladSerhiienko <vlad.serhiienko@gmail.com>
2015-05-22 01:49:28 +0300

    labwork4&5: added results for labwork4, added reports, results and code for labwork5

---
+++ Serhiienko/Serhiienko_lab5/Labworks.Console/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GAF;
+using GAF.Operators;
+using Labworks.Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Concurrent;
+
+namespace Labworks.ConsoleApp
+{
+    class SolverHooks
+    {
+        ConcurrentQueue<string> fileContent;
+
+        List<City> cityList = City.Cities.ToList();
+        internal void LaunchAndTrace(TravellingSalesmanProblemSolver solver)
+        {
+            fileContent = new ConcurrentQueue<string>();
+            solver.EvaluateFitness += TSPSolverOnEvaluateFitness;
+            solver.GenerationComplete += TSPSolverOnGenerationComplete;
+
+            var fileStreamSlns = new StreamWriter("cities-solutions.txt");
+
+            int slnInd = 0;
+            foreach (var solution in solver.Solve(cityList))
+            {
+                fileStreamSlns.WriteLine(string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}"
+                     , "s"
+                     , ""
+                     , solution.Distance
+                     , solution.Fitness
+                     , slnInd
+                     , solution.CityIndexString
+                     , solution.CityCoordsString
+                     ));
+                slnInd++;
+            }
+            fileStreamSlns.Close();
+
+
+            int gi = 0;
+            bool openNewFile = true;
+
+            StreamWriter fileStreamGnrs = null;
+            foreach (var entry in fileContent)
+            {
+                switch (entry[0])
+                {
+                    case 'c':
+                        if (openNewFile)
+                        {
+                            if (fileStreamGnrs != null)
+                                fileStreamGnrs.Close();
+                            fileStreamGnrs = new StreamWriter("cities-generation" + gi++ + ".txt");
+                            openNewFile = false;
+                        }
+
+                        fileStreamGnrs.WriteLine(entry);
+                        break;
+                    case 'g':
+                        openNewFile = true;
+                        fileStreamGnrs.WriteLine(entry);
+                        break;
+                }
+            }
+
+            fileStreamGnrs.Close();
+        }
+
+        private void TSPSolverOnGenerationComplete(GaEventArgs args)
+        {
+            Console.Write('*');
+
+            int slnInd = 0;
+            foreach (var item in args.Population.Solutions)
+            {
+                if (!item.IsElite) continue;
+                var solution = new TravellingSalesmanProblemSolution(item, cityList);
+                fileContent.Enqueue(string.Format("g{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}"
+                    , args.Generation
+                    , ""
+                    , solution.Distance
+                    , solution.Fitness
+                    , slnInd
+                    , solution.CityIndexString
+                    , solution.CityCoordsString
+                    ));
+                ++slnInd;
+            }
+        }
+
+        private void TSPSolverOnEvaluateFitness(Chromosome chromosome, TravellingSalesmanProblemSolution solution)
+        {
+            Console.Write('.');
+            fileContent.Enqueue(string.Format("c\t{0}\t{1}\t{2}\t{3}\t{4}"
+                , chromosome.ToString()
+                , solution.Distance
+                , solution.Fitness
+                , solution.CityIndexString
+                , solution.CityCoordsString
+                ));
+        }
+    }
+
+    class Program
+    {
+        static void LaunchLabwork5()
+        {
+            var hooks = new SolverHooks();
+            hooks.LaunchAndTrace(new TravellingSalesmanProblemSolver());
+        }
+
+        static void Main(string[] args)
+        {
+            LaunchLabwork5();
+        }
+    }
+}
---
+++ Serhiienko/Serhiienko_lab5/Labworks.ExcelAddIn/EvolutionaryAlgorithmsRibbon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace Labworks.ExcelAddIn
+{
+    public partial class EvolutionaryAlgorithmsRibbon
+    {
+        private void EvolutionaryAlgorithmsRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void TSPCharts_Click(object sender, RibbonControlEventArgs e)
+        {
+            var wb = Globals.ThisAddIn.Application.ActiveWorkbook;
+            var ws = wb.ActiveSheet as Worksheet;
+            var rowCount = ws.UsedRange.Rows.Count;
+            object misValue = System.Reflection.Missing.Value;
+
+            int rowHeight = 15;
+
+            //ws.UsedRange.Rows.RowHeight = 20;
+
+            int currentRow = 1;
+            while (currentRow <= rowCount)
+            {
+                var value = ws.Range["A" + currentRow].Value;
+                if (value == null) break;
+
+                var nameValue = ws.Range["B" + currentRow].Value;
+
+                char entry = ((string)value)[0];
+                string name = ((string)nameValue);
+                if (entry == 'c' || entry == 'g' || entry == 's')
+                {
+                    double l = 240;
+                    double t = currentRow * rowHeight;
+                    double h = 16 * rowHeight;
+                    double w = h * 1.5;
+
+                    Excel.ChartObjects charts = (Excel.ChartObjects)ws.ChartObjects(Type.Missing);
+                    Excel.ChartObject chart = (Excel.ChartObject)charts.Add(l, t, w, h);
+                    Excel.Chart chartPage = chart.Chart;
+
+                    chartPage.ChartType = Excel.XlChartType.xlXYScatterLines;
+                    chartPage.ChartStyle = 205;
+
+                    Excel.SeriesCollection seriesCollection = chartPage.SeriesCollection();
+                    Excel.Series series1 = seriesCollection.NewSeries();
+                    series1.XValues = ws.Range["C" + (currentRow + 1) + ":C" + (currentRow + 16)];
+                    series1.Values = ws.Range["D" + (currentRow + 1) + ":D" + (currentRow + 16)];
+                    series1.Name = name + " ";
+
+                    series1.Name += "Distance: " + ((double)ws.Range["C" + currentRow].Value).ToString();
+                    series1.Name += ", Fitness: " + ((double)ws.Range["D" + currentRow].Value).ToString();
+
+                    currentRow += 17;
+                }
+
+                currentRow++;
+            }
+
+
+
+
+
+        }
+    }
+}
