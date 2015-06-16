55e40491700cf4999174ec0ba14deb5e50b7cb2f
Yura Rybak <yurarybak1993@gmail.com>
2015-06-16 10:57:16 +0300

    Lab2
    Lab2

--- Rybak_Lab3/create_permutations.m
+++
@@ -1,9 +0,0 @@
-function pop = create_permutations(NVARS,FitnessFcn,options)
-%функція для створення популяції
-totalPopulationSize = sum(options.PopulationSize);
-n = NVARS;
-pop = cell(totalPopulationSize,1);
-for i = 1:totalPopulationSize
-    pop{i} = randperm(n); 
-end
-
